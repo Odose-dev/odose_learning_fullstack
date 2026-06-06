ec9ed5d7e6f95905aabc653fcc075c0e67fb251b
Odoseiye Esese <odose@MacBookAir.fritz.box>
2026-06-06 20:04:15 +0200

    Old method for writing functions

--- devzeke/main.js
+++ devzeke/main.js
@@ -1,5 +1,5 @@
 // class 1
-
+/*
 // old was creating obj
 let user = {
     "firstname": "Ezekiel",
@@ -7,14 +7,16 @@ let user = {
 }
 
 //Object Shorthand
+/*
 const {firstname, field} = user
 
 //outputs old
+/*
 console.log(firstname)
 console.log(field)
 
 //Computed Properties
-
+/*
 const email = "tope@gmail.com";
 const pass = "mypass"
 
@@ -24,30 +26,39 @@ const anotherUser = {
 }
 
 console.log(anotherUser.pass)
-
+*/
+//Optional Chaining
+/*
 const emailValue = undefined ?? 'Gest';
 console.log(emailValue)
+*/
 
-//Optional Chaining
 
 //spead & rest
+/*
 const team1 =["tope", "tola", "tosin"]
 const team2 =["Ezekiel", "odose", "tayo"]
-
-
-
 const group = [...team1, ...team2]
-
 console.log(group);
+*/
 //
 
 
 //class 2 
 
 //anonymous function / Arrow Functions
+
+
     //old ways
+ function add2Numbers(a, b){
+    return a+b;
+ }
+
+ console.log(add2Numbers(4,8))
+
 
     //new ways
+    
 
     //hoisting
 
